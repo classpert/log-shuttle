0ed5e4a134d0a9c15b24f1771143710367f0fc75
Thiago Brandão <thiagobrandam@gmail.com>
2020-04-09 18:10:41 -0300

    Fix entrypoint url

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Example: curl -N "http://172.17.0.2/logs" | /bin/log-shuttle -logs-url=http://webhook.site/a26ae22d-0cf8-491a-8a32-0aa8907c3318
-if [ -z "$LOGSPLOUT_URL" ]
+if [ -z "$LOGSPOUT_URL" ]
 then
-	echo "Please set \$LOGSPLOUT_URL"
+	echo "Please set \$LOGSPOUT_URL"
 elif [ -z "$LOGPLEX_INPUT_URL" ]
 then
 	echo "Please set \$LOGPLEX_INPUT_URL"
 else
-	curl -sSN ${LOGSPLOUT_URL} | /bin/log-shuttle -logs-url=${LOGPLEX_INPUT_URL} -logplex-token={$LOGPLEX_TOKEN}
+	curl -sSN ${LOGSPOUT_URL} | /bin/log-shuttle -logs-url=${LOGPLEX_INPUT_URL} -logplex-token={$LOGPLEX_TOKEN}
 fi
